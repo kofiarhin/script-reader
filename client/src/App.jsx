14c9fb914e2123f8fcfee36db4d390dc72e0dcb5
kofi arhin <kofiarhin69@gmail.com>
2025-06-28 06:11:33 +0100

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.styles.scss";
+const App = () => {
+  return (
+    <div>
+      <section id="heading">
+        <div className="container">
+          <h1 className="heading center">Script Reader</h1>
+          <p className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            voluptatibus hic odio repudiandae architecto facere, modi, voluptate
+            nihil asperiores quasi ullam corrupti non in adipisci culpa maxime.
+            Optio, dolores eaque dolor sapiente id quae mollitia quaerat vero
+            fuga beatae expedita itaque, suscipit autem eum voluptas tempore,
+            aut ipsam neque temporibus? Temporibus libero explicabo, nobis
+            dolorem animi cumque eveniet facilis nam ad, non eius maiores neque
+            totam blanditiis laboriosam quos veritatis odit? Quas placeat vel
+            totam voluptate blanditiis voluptatibus, delectus quidem nulla
+            aperiam fugit nostrum quisquam, nisi cum repellat eius reprehenderit
+            vero numquam ex eligendi at facilis inventore explicabo quibusdam
+            alias?
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default App;
